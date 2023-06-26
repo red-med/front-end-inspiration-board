1bf04501b1c8c67c09c9bed90c0a7311f2c36742
Kira <kira.kaur.sidhu@gmail.com>
2023-06-26 14:26:21 -0700

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+
+
+const BOARDS = [{id: 1,
+                title: "Board 1",
+                owner: "Kira"},
+                {id: 2,
+                title: "Board 2",
+                owner: "Rediet"}
+              ]
+
+const CARDS = [{id: 1,
+                message: "Test message",
+                likes_count: 3,
+                date_created: "1/1/23"}
+              ]
 
 function App() {
+  const [boards, setBoards] = useState(BOARDS);
+  const [currentBoard, setCurrentBoard] = useState(BOARDS[0]);
+  const [cards, setCards] = useState(CARDS);
+  const [currentCard, setCurrentCard] = useState(CARDS[0]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Inspiration Board</h1>
       </header>
     </div>
   );
